85e8ebd99e4bec2b296c362347de8edc6dbf068b
Jagwani <nitin.jagwani@refinitiv.com>
2021-06-08 10:27:04 +0400

    Updates to structure, created main.js file

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,22 @@
-window.addEventListener('DOMContentLoaded', (event) =>{
+window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
-})
+});
 
-const functionApi = '';
+
+const localApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
-        return response.json(}
-    }).then(response =>{
-        console.log("Website called fuction API.");
+    fetch(functionApi)
+    .then(response => {
+        return response.json()
+    })
+    .then(response => {
+        console.log("Website called function API.");
         count = response.count;
-        document.getElementById("counter").innerText = count;
-    }).cat(function(error){
+        document.getElementById('counter').innerText = count;
+    }).catch(function(error) {
         console.log(error);
-    });
+      });
     return count;
 }
