7fab08b0b2a0060a43aed510519e238ad34be464
Jagwani <nitin.jagwani@refinitiv.com>
2021-06-28 16:15:23 +0400

    Deployed Storage, Function and made changes to frontend

--- backend/api/GetResumeCounterNitin.cs
+++ backend/api/GetResumeCounterNitin.cs
@@ -19,8 +19,8 @@ namespace Company.Function
         [FunctionName("GetResumeCounterNitin")]
         public static HttpResponseMessage Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            [CosmosDB(databaseName:"Azureresume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
-            [CosmosDB(databaseName:"Azureresume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter, 
+            [CosmosDB(databaseName:"AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
+            [CosmosDB(databaseName:"AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter, 
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
